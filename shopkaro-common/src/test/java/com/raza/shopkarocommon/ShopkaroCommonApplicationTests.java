2980a6ea750a648863551c318a2ed4eb8dc07e87
AASIF RAZA <50637795+razaaasif@users.noreply.github.com>
2024-01-15 18:34:54 +0530

    created all project and subprojects

---
+++ shopkaro-common/src/test/java/com/raza/shopkarocommon/ShopkaroCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raza.shopkarocommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopkaroCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ shopkaro-webapp/src/test/java/com/raza/shopkaro/ShopkaroWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raza.shopkaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopkaroWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
